059695dc560271d071f643d1857c75f00838e821
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2017-02-12 14:38:27 +0100

    Further improved preparation.sh

--- bin/preparation.sh
+++ bin/preparation.sh
@@ -1,67 +1,82 @@
 #!/bin/bash
   
-  #Search for config files, if they don't exist, copy the default ones
-  if [ ! -f /config/php.ini ]; then
-    echo "copying php.ini"
-    cp  /etc/php5/apache2/php.ini /config/php.ini
-  else
-    echo "php.ini already exists"
-    cp /config/php.ini /etc/php5/apache2/php.ini
-  fi
+echo "Preparing data folder"
+if [ ! -d /data/data ]; then
+  echo "Preparing data folder"
+  mkdir /data/data
+  mkdir /data/data/events
+  mkdir /data/data/images
+  mkdir /data/data/temp
+else
+  echo "Using existing data directory"
+  mv /usr/share/zoneminder/www/events /data/data/
+  mv /usr/share/zoneminder/www/images /data/data/
+  mv /usr/share/zoneminder/www/temp /data/data/
+fi
+echo "Linking data directories ..."
+ln -s /data/data/events /usr/share/zoneminder/www/events
+ln -s /data/data/images /usr/share/zoneminder/www/images
+ln -s /data/data/temp /usr/share/zoneminder/www/temp
   
-  # Copy mysql database if it doesn't exit
-  if [ ! -d /config/mysql/mysql ]; then
-    echo "moving mysql to config folder"
-    rm -r /config/mysql
-    cp -p -R /var/lib/mysql /config/
-  else
-    echo "using existing mysql database"
-  fi
+echo "Preparing mysql folder"
+if [ ! -d /data/mysql/mysql ]; then
+  echo "Moving mysql to data folder"
+  rm -r /data/mysql
+  cp -p -R /var/lib/mysql /data/
+  echo "Adding default Zoneminder database settings"
+  mysql -uroot < /usr/share/zoneminder/db/zm_create.sql 
+  mysql -uroot -e "grant all on zm.* to 'zmuser'@localhost identified by 'zmpass';" 
+else
+  echo "Using existing mysql database"
+  mv /var/lib/mysql /data/
+fi  
+ln -s /data/mysql /var/lib/mysql
   
-  # Copy data folder if it doesn't exist
-  if [ ! -d /config/data ]; then
-    echo "moving data folder to config folder"
-    mkdir /config/data
-    mkdir /config/data/images
-    mkdir /config/data/events
-    mkdir /config/data/temp
-  else
-    echo "using existing data directory"
-  fi
-  
-  if [ ! -d /config/perl5 ]; then
-    echo "moving perl data folder to config folder"
-    mkdir /config/perl5
-    cp -R -p /usr/share/perl5/ZoneMinder /config/perl5/
-  else
-    echo "using existing perl data directory"
-  fi
+echo "Preparing php.ini"
+if [ ! -f /data/php.ini ]; then
+  echo "Copying default php.ini and set time zone to Europe Berlin"
+  mv  /etc/php5/apache2/php.ini /data/php.ini
+  sed  -i 's/\;date.timezone =/date.timezone = \"Europe\/Berlin\"/' /data/php.ini 
+else
+  echo "php.ini already exists"
+fi
+ln -s /data/php.ini /etc/php5/apache2/php.ini
 
-  
-  echo "creating symbolink links"
-  rm -r /usr/share/zoneminder/www/events
-  rm -r /usr/share/zoneminder/www/images
-  rm -r /usr/share/zoneminder/www/temp
-  rm -r /var/lib/mysql
-  rm -r /usr/share/perl5/ZoneMinder
-  ln -s /config/data/events /usr/share/zoneminder/www/events
-  ln -s /config/data/images /usr/share/zoneminder/www/images
-  ln -s /config/data/temp /usr/share/zoneminder/www/temp
-  ln -s /config/mysql /var/lib/mysql
-  ln -s /config/perl5/ZoneMinder /usr/share/perl5/ZoneMinder
-  chown -R mysql:mysql /var/lib/mysql
-  chown -R www-data:www-data /config/data
-  chmod -R go+rw /config
-  
-  
-  mysql -uroot < /usr/share/zoneminder/db/zm_create.sql && \
-mysql -uroot -e "grant all on zm.* to 'zmuser'@localhost identified by 'zmpass';" && \
-service mysql restart && \
-chmod 740 /etc/zm/zm.conf && \
-chown root:www-data /etc/zm/zm.conf && \
+echo "Preparing perl5 folder"
+if [ ! -d /data/perl5 ]; then
+  echo "Moving perl5 folder to data folder"
+  mkdir /data/perl5
+  cp -R -p /usr/share/perl5/ZoneMinder /data/perl5/
+else
+  echo "Using existing perl5 data directory"
+  mv /usr/share/perl5/ZoneMinder /data/perl5/
+fi
+ln -s /data/perl5/ZoneMinder /usr/share/perl5/ZoneMinder
 
-chown -R www-data:www-data /usr/share/zoneminder/ && \
-sed  -i 's/\;date.timezone =/date.timezone = \"America\/New_York\"/' /etc/php5/apache2/php.ini && \
+echo "Preparing SSL cert and key"
+if [ -f /data/ssl-certs/zoneminder-key.pem ] ; then
+  echo "External zoneminder-key.pem exists"
+  rm -f /data/ssl-certs/zoneminder-key.pem.missing
+else
+  echo "Missing zoneminder-key.pem, create empty one"
+  echo "Add zoneminder-key.pem key in this folder" > /data/ssl-certs/zoneminder-key.pem.missing
+fi
+if [ -f /data/ssl-certs/zoneminder-fullchain.pem ] ; then
+  echo "External zoneminder-fullchain.pem exists"
+  rm -f /data/ssl-certs/zoneminder-fullchain.pem.missing
+else
+  echo "Missing zoneminder-fullchain.pem, create empty one"
+  echo "Add zoneminder-fullchain.pem key in this folder" > /data/ssl-certs/zoneminder-fullchain.pem.missing
+fi
+  
+echo "Fix folder permissions"
+chown -R mysql:mysql /var/lib/mysql
+chown -R www-data:www-data /data/data
+chmod -R go+rw /data
+chmod 740 /etc/zm/zm.conf 
+chown root:www-data /etc/zm/zm.conf 
+chown -R www-data:www-data /usr/share/zoneminder/ 
+
   
   
   
